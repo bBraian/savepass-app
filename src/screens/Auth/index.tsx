4cd1185919158ac86e3ccd17077e3b8d718a7552
bBraian <braianvoficial@gmail.com>
2023-02-19 19:49:22 -0300

    adding biometry validation

--- src/screens/Auth/index.tsx
+++ src/screens/Auth/index.tsx
@@ -1,8 +1,12 @@
 import { useNavigation } from "@react-navigation/native";
-import { Container } from "./styles";
+import * as LocalAuthentication from "expo-local-authentication";
+import { AuthButton, ButtonTitle, Container, Hello, Logo, Title, UserName } from "./styles";
 
 import { StackNavigationProp } from '@react-navigation/stack';
-import { Button, Text } from "react-native";
+import { Alert, Button } from "react-native";
+
+import img from '../../../assets/images/adaptive-icon.png';
+import { useEffect, useState } from "react";
 
 type RootStackParamList = {
     Home: undefined;
@@ -12,16 +16,79 @@ type RootStackParamList = {
 type NavigationProps = StackNavigationProp<RootStackParamList>;
 
 export function Auth() {
+    const [fingerPrintSupported, setFingerPrintSupported] = useState(false);
     const { navigate } = useNavigation<NavigationProps>();
-
+    
     function handleSignInWithGoogle() {
         navigate('Home');
     }
 
+    function fallBackToDefaultAuth() {
+
+    }
+
+    function alertComponent(title: string, message: string, btnText: string, btnFunction: () => void) {
+        return Alert.alert(title, message), [
+            { text: btnText, onPress: btnFunction }
+        ]
+    }
+
+    async function handleFingerPrintAuth() {
+        const isAuth = await LocalAuthentication.hasHardwareAsync();
+        if(!isAuth) {
+            return alertComponent(
+                "Insira sua senha",
+                "Autenticacao por biometria",
+                "Ok",
+                () => fallBackToDefaultAuth()
+            );
+        }
+
+        let biometricSupport;
+
+        if(isAuth) {
+            biometricSupport = await LocalAuthentication.supportedAuthenticationTypesAsync();
+        }
+        const saveBiometrics = await LocalAuthentication.isEnrolledAsync();
+
+        if(!saveBiometrics) {
+            return alertComponent(
+                "Insira sua senha",
+                "Autenticacao por biometria",
+                "Ok",
+                () => fallBackToDefaultAuth()
+            );
+        }
+
+        const bioAuth = await LocalAuthentication.authenticateAsync({
+            promptMessage: 'Entrar com biometria',
+            cancelLabel: 'Cancelar',
+            disableDeviceFallback: true
+        })
+
+        if(bioAuth) {
+            console.log('Sucess')
+        }
+    }
+    
+    useEffect(() => {
+        async function verifyFingerPrintSupport() {
+            const compatible = await LocalAuthentication.hasHardwareAsync();
+            setFingerPrintSupported(compatible);
+        }
+
+        verifyFingerPrintSupport();
+    }, [])
     return (
         <Container>
-            <Text>auth</Text>
-            <Button onPress={() => handleSignInWithGoogle}>Login</Button>
+            <Logo source={img} />
+            <Title>
+                <Hello>Ola, </Hello>
+                <UserName>Braian</UserName>
+            </Title>
+            <AuthButton onPress={handleFingerPrintAuth}>
+                <ButtonTitle>Entrar</ButtonTitle>
+            </AuthButton>
         </Container>
     )
 }
--- src/screens/Auth/styles.ts
+++ src/screens/Auth/styles.ts
@@ -4,6 +4,39 @@ import { RFValue } from 'react-native-responsive-fontsize';
 export const Container = styled.View`
   flex: 1;
   background-color: #624ee4;
-  justify-content: center;
+  justify-content: space-between;
+  padding: 120px 0;
   align-items: center;
 `;
+
+export const Logo = styled.Image`
+    width: ${RFValue(120)}px;
+    height: ${RFValue(120)}px;
+`;
+
+export const Title = styled.View`
+    flex-direction: row;
+`;
+
+export const Hello = styled.Text`
+    font-family: 'Rubik_400Regular';
+    font-size: ${RFValue(30)}px;
+    text-align: center;
+    color: #ffffff;
+`;
+
+export const UserName = styled.Text`
+    font-family: 'Rubik_500Medium';
+    font-size: ${RFValue(30)}px;
+    text-align: center;
+    color: #ffffff;
+`;
+
+export const ButtonTitle = styled.Text`
+    font-family: 'Rubik_500Medium';
+    font-size: ${RFValue(30)}px;
+    text-align: center;
+    color: #ffffff;
+`;
+
+export const AuthButton = styled.TouchableOpacity``;
